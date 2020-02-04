11aca2d313762a9ee7d583b721ec052e79f46881
Alishan Piyarali <48108101+alishanP@users.noreply.github.com>
2020-02-03 19:56:05 -0500

    Add files via upload

---
+++ userInfo.py
@@ -0,0 +1,21 @@
+myInfo = {
+    "nikeOrAdidas" : 1, # 1 for nike, 2 for Adidas
+    "SNKRS?" : True, # True If product is on Nike SNKRS, false otherwise
+    "productURL" : "https://www.nike.com/ca/launch/t/womens-air-jordan-5-iridescent-oil-grey/", # product URL
+    "USsize" : 6, # Enter size, S, M, L.. ext. Any shoe size
+    "mensOrWomens" : "M", # M for men, W for women
+    "firstName": "Alishan",  #  first name
+    "lastName": "Piyarali", #  last name
+    "phone" : 4168203706, #  phone number (no hyphens)
+    "email": "alishan.piyarali@gmail.com",  #  email address
+    "address1" : "100 City Centre Dr", #  home address
+    "address2": 555,  #  secondary address
+    "city": "Mississauga",  # city
+    "province": "Ontario",  #province
+    "postCode" : "L5B2C9", # zip code
+    "cardNumber" : 4506-4457-9123-4567, # card number
+    "cardType" : "Visa", # insert card type
+    "cardMonth" : 05,   #expiry month
+    "cardYear": 2022,   #expiry year
+    "cvv" : 555, # card cvv
+}
